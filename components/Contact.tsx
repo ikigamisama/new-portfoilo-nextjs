8377b502a44ad1da6637194aaa5d1fc3e8107b8d
FranzGoAcquirely <franz@goacquirely.com>
2023-03-18 00:58:32 +0800

    Added contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,11 @@
-import { SecTitleText } from "@/styles/About";
+import { AboutMeHeadingText, SecTitleText } from "@/styles/About";
+import { ContactCardWrapper, ContactTextHead } from "@/styles/Contact";
 import { MainSectionVidWrap } from "@/styles/MainSection";
 import { VideoSkillsSection } from "@/styles/Skills";
-import { Box, Container } from "@chakra-ui/react";
-import { Roboto_Mono } from "next/font/google";
+import { Box, Container, HStack, Image } from "@chakra-ui/react";
+import { Roboto_Mono, Space_Mono } from "next/font/google";
+
+import contactMeImg from "@/assets/img/contact-me.jpg";
 
 const font_roboto = Roboto_Mono({
   weight: ["300", "400", "700"],
@@ -12,7 +15,7 @@ const font_roboto = Roboto_Mono({
 
 export default function Contact({ colorMode }: any) {
   return (
-    <Box as="section" py="75" position="relative" id="contact">
+    <Box as="section" position="relative" py="75" id="contact">
       <MainSectionVidWrap>
         <VideoSkillsSection
           src={"contacts.mp4"}
@@ -26,6 +29,12 @@ export default function Contact({ colorMode }: any) {
           &#60;contact&#62;
         </SecTitleText>
 
+        <ContactTextHead className={font_roboto.className}>
+          Feel free to drop to my email{" "}
+          <a href="mailto:ikigamidevs.15@gmail.com">ikigamidevs.15@gmail.com</a>
+        </ContactTextHead>
+
+        <Image src={contactMeImg.src} w="100%" />
         <SecTitleText className={font_roboto.className}>
           &#60;/contact&#62;
         </SecTitleText>
---
+++ styles/Contact.ts
@@ -0,0 +1,25 @@
+import { Box, Text } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+
+export const ContactCardWrapper = styled(Box)`
+width: 100%;
+height: 100%;
+${(props? : any) => (props.theme === 'light' ? 'background-color: white;' : 'background-color: #202020;')};
+`
+
+
+export const ContactTextHead = styled(Text)`
+    font-size: 30px;
+    margin-bottom: 25px;
+    font-weight: 300;
+    font-style: italic;
+    color: ${(props? : any) => (props.theme === 'light' ? '#202020' : 'white')};
+
+
+    & > a{
+        text-decoration: underline;
+        text-underline-offset: 5px;
+        font-weight: 500;
+    }
+`
